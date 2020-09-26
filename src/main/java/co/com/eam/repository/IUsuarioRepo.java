69b7152cc0e79443e34f7298ae8d7618c3484afd
sergiodazaguzman01 <57229469+sergiodazaguzman01@users.noreply.github.com>
2020-09-25 23:54:03 -0500

    Verificando que todo este correcto

--- src/main/java/co/com/eam/repository/IUsuarioRepo.java
+++ src/main/java/co/com/eam/repository/IUsuarioRepo.java
@@ -1,10 +1,14 @@
 package co.com.eam.repository;
 
 
+import java.util.List;
+
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import co.com.eam.domain.Bodega;
 import co.com.eam.domain.Usuario;
 
 
@@ -13,6 +17,12 @@ public interface IUsuarioRepo extends
 CrudRepository<Usuario, Integer>{
 	
 	@Query("SELECT u From Usuario u WHERE u.username=?1 and u.contrasena=?2")
-	Usuario Login(String nombre_usuario, String contrasena);
+	Usuario Login(String username, String contrasena);
+	
+	@Query("Select u from Usuario u WHERE u.municipio.id_municipio=?1")
+	List<Usuario> listarmunicipio(int idMunicipio);
+	
+	@Query("SELECT u FROM Usuario u WHERE u.nombre=?1")
+	List<Usuario> BuscarUsuarioNombre(String nombre);
 	
 }
