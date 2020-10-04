9374248a3de3c511cc6c93dffd52486955fd9662
juandavidgaitan <juan.gaitan.9656@eam.edu.co>
2020-10-04 10:23:54 -0500

    modificar la bodega controlador

--- src/main/java/co/com/eam/controller/BodegaControllerViejo.java
+++ src/main/java/co/com/eam/controller/BodegaControllerViejo.java
@@ -2,6 +2,7 @@ package co.com.eam.controller;
 
 import javax.validation.Valid;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
