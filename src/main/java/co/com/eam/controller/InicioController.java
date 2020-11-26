d6d9b6272a22844eb1fe766008e6a34a0946567a
tiagoking <71114540+tiagoking@users.noreply.github.com>
2020-11-26 18:11:57 -0500

    Agregue el home page SERGIO

--- src/main/java/co/com/eam/controller/InicioController.java
+++ src/main/java/co/com/eam/controller/InicioController.java
@@ -113,7 +113,7 @@ public class InicioController {
 		return "index-administrador";
 	}
 	
-	@GetMapping("/")
+	@GetMapping("/producto")
 	public String login(@RequestParam Map <String, Object> params,Usuario usuario, Model model) {
 		int page = params.get("page") != null ? (Integer.valueOf(params.get("page").toString()) - 1) : 0;
 		PageRequest pageRequest = PageRequest.of(page,3);
@@ -139,6 +139,11 @@ public class InicioController {
 	public String login1(Usuario usuario, Model model) {
 	 	model.addAttribute("usuario", new Usuario());
 		return "login";
+	}
+	
+	@GetMapping("/")
+	public String lhola(Model model) {
+		return "index";
 	}
 	
 	@SuppressWarnings("unused")
